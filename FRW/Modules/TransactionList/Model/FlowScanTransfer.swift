349c3f324355c8505b513dd107c65d68e9dc76ca
cat <zhouxl72@126.com>
2024-02-28 11:30:19 +0800

    fix:  show data on transfer

--- FRW/Modules/TransactionList/Model/FlowScanTransfer.swift
+++ FRW/Modules/TransactionList/Model/FlowScanTransfer.swift
@@ -84,12 +84,8 @@ struct FlowScanTransfer: Codable {
     }
     
     var amountString: String {
-        let f = NumberFormatter()
-        f.maximumFractionDigits = 8
-        f.minimumFractionDigits = 0
-        f.roundingMode = .halfUp
-        if let amountString = self.amount, let doubleAmount = Double(amountString), let finalString = f.string(for: NSNumber(value: doubleAmount / 100000000.0).decimalValue) {
-            return finalString
+        if let amountString = self.amount, !amountString.isEmpty {
+            return amountString
         } else {
             return "-"
         }
