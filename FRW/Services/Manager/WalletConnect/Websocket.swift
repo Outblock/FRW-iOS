daa7ec905e90af0d66702753a043b782eaa487f2
cat <zhouxl72@126.com>
2024-06-06 21:32:53 +0800

    feat： reset pre vision

--- FRW/Services/Manager/WalletConnect/Websocket.swift
+++ FRW/Services/Manager/WalletConnect/Websocket.swift
@@ -9,63 +9,13 @@ import Foundation
 import Starscream
 import WalletConnectRelay
 
+extension WebSocket: WebSocketConnecting { }
 
-class FlowWebSocket: WebSocket, WebSocketConnecting {
-    var isConnected: Bool = false
-    
-    var onConnect: (() -> Void)?
-    
-    var onDisconnect: (((any Error)?) -> Void)?
-    
-    var onText: ((String) -> Void)?
-    
-    
-}
-
-
-class SocketFactory: WebSocketFactory, WebSocketDelegate {
-    
-    var socket: FlowWebSocket?
-    
+class SocketFactory: WebSocketFactory {
+    var socket: WebSocket?
     func create(with url: URL) -> WebSocketConnecting {
-        var request = URLRequest(url: url)
-        request.timeoutInterval = 5
-        let socket = FlowWebSocket(request: request)
-        socket.delegate = self
+        let socket = WebSocket(url: url)
         self.socket = socket
         return socket
-    }
-    
-    func didReceive(event: Starscream.WebSocketEvent, client: any Starscream.WebSocketClient) {
-        switch event {
-            case .connected(let headers):
-            socket?.isConnected = true
-            socket?.onConnect?()
-            
-            case .disconnected(let reason, let code):
-            socket?.isConnected = false
-            socket?.onDisconnect?(nil)
-            log.info("[SocketFactory] websocket is disconnected: \(reason) with code: \(code)")
-            case .text(let string):
-            socket?.onText?(string)
-            log.info("[SocketFactory] Received text: \(string)")
-            case .binary(let data):
-                print("Received data: \(data.count)")
-            case .ping(_):
-                break
-            case .pong(_):
-                break
-            case .viabilityChanged(_):
-                break
-            case .reconnectSuggested(_):
-                break
-            case .cancelled:
-            socket?.isConnected = false
-            case .error(let error):
-            socket?.isConnected = false
-                
-                case .peerClosed:
-                       break
-            }
     }
 }
