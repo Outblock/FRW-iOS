b6c6e86d3c9c6b7c20a7ac7e36654f7230168b5d
cat <zhouxl72@126.com>
2024-08-30 13:52:29 +0800

    feat： update token

--- FRW/Services/Network/GithubEndpoint.swift
+++ FRW/Services/Network/GithubEndpoint.swift
@@ -24,14 +24,26 @@ extension GithubEndpoint: TargetType {
         case .collections:
             return "/Outblock/Assets/main/nft/nft.json"
         case .ftTokenList:
-            switch LocalUserDefaults.shared.flowNetwork {
-            case .testnet:
-                return "/Outblock/token-list-jsons/outblock/jsons/testnet/flow/reviewers/0xa51d7fe9e0080662.json"
-            case .mainnet:
-                return "/Outblock/token-list-jsons/outblock/jsons/mainnet/flow/reviewers/0xa2de93114bae3e73.json"
-            case .previewnet:
-                return "/Outblock/token-list-jsons/outblock/jsons/previewnet/flow/default.json"
+            if isDevModel {
+                switch LocalUserDefaults.shared.flowNetwork {
+                case .mainnet:
+                    return "/Outblock/token-list-jsons/outblock/jsons/mainnet/flow/dev.json"
+                case .testnet:
+                    return "/Outblock/token-list-jsons/outblock/jsons/testnet/flow/dev.json"
+                case .previewnet:
+                    return "/Outblock/token-list-jsons/outblock/jsons/previewnet/flow/default.json"
+                }
+            }else {
+                switch LocalUserDefaults.shared.flowNetwork {
+                case .mainnet:
+                    return "/Outblock/token-list-jsons/outblock/jsons/mainnet/flow/default.json"
+                case .testnet:
+                    return "/Outblock/token-list-jsons/outblock/jsons/testnet/flow/default.json"
+                case .previewnet:
+                    return "/Outblock/token-list-jsons/outblock/jsons/previewnet/flow/default.json"
+                }
             }
+            
         case .EVMNFTList:
             switch LocalUserDefaults.shared.flowNetwork {
             case .testnet:
