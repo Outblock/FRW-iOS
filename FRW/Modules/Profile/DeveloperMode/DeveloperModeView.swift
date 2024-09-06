ba3fdb6d1d1a387375449a110c3b2f6b5b588509
cat <zhouxl72@126.com>
2024-09-06 08:18:40 +0800

    feat: remove Develop Model EVM enable

--- FRW/Modules/Profile/DeveloperMode/DeveloperModeView.swift
+++ FRW/Modules/Profile/DeveloperMode/DeveloperModeView.swift
@@ -19,7 +19,7 @@ struct DeveloperModeView: RouteableView {
     @StateObject private var walletManager = WalletManager.shared
     
     @AppStorage("isDeveloperMode") private var isDeveloperMode = false
-    @AppStorage("EVMEnable") private var isEVMEnable = false
+    
     
     @State private var openLogWindow = LocalUserDefaults.shared.openLogWindow
     
@@ -152,15 +152,6 @@ struct DeveloperModeView: RouteableView {
                         .padding(.vertical, 8)
                     VStack(spacing: 0) {
                         Section {
-                            HStack {
-                                Toggle("evm_on_flow".localized, isOn: $isEVMEnable)
-                                    .toggleStyle(SwitchToggleStyle(tint: .LL.Primary.salmonPrimary))
-                                    .onChange(of: isEVMEnable) { _ in
-                                        EVMAccountManager.shared.updateWhenDevChange()
-                                    }
-                            }
-                            .frame(height: 64)
-                            .padding(.horizontal, 16)
                             
                             HStack {
                                 Button {
