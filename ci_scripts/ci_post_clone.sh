a03662ff3549a8486f39ac4bf8f76cb272929b3e
Antonio Bello <1085903+jeden@users.noreply.github.com>
2024-11-18 23:31:31 +0100

    - Used encoded files as env variables, generated on the fly post-clone

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,17 +7,59 @@ echo "Stage: PRE-Xcode Build running.... "
 
 cd ../FRW/App/Env/ 
 
-FILE=LocalEnv
+LOCAL_ENV_FILE=./LocalEnv
 
-if [ ! -f $FILE ]
+GOOGLE_OAUTH2_FILE_DEV=./Dev/GoogleOAuth2.plist
+GOOGLE_SERVICE_FILE_DEV=./Dev/GoogleService-Info.plist
+SERVICE_CONFIG_FILE_DEV=./Dev/ServiceConfig.plist
+
+GOOGLE_OAUTH2_FILE_PROD=./Prod/GoogleOAuth2.plist
+GOOGLE_SERVICE_FILE_PROD=./Prod/GoogleService-Info.plist
+SERVICE_CONFIG_FILE_PROD=./Prod/ServiceConfig.plist
+
+
+if [ ! -f $LOCAL_ENV_FILE ] 
 then
-	cp _LocalEnv LocalEnv
-	sed -i bak -e "s|\$WalletConnectProjectID|$WalletConnectProjectID|g" LocalEnv
-	sed -i bak -e "s|\$BackupAESKey|$BackupAESKey|g" LocalEnv
-	sed -i bak -e "s|\$AESIV|$AESIV|g" LocalEnv
-	sed -i bak -e "s|\$TranslizedProjectID|$TranslizedProjectID|g" LocalEnv
-	sed -i bak -e "s|\$TranslizedOTAToken|$TranslizedOTAToken|g" LocalEnv
+	echo "Generating LocalEnv..."
+	base64 -D -o $LOCAL_ENV_FILE <<< $LOCAL_ENV
 fi
+
+if [ ! -f $GOOGLE_OAUTH2_FILE_DEV ] 
+then
+	echo "DEV: Generating GoogleOAuth2..."
+	base64 -D -o $GOOGLE_OAUTH2_FILE_DEV <<< $GOOGLE_OAUTH2_DEV
+fi
+
+if [ ! -f $GOOGLE_SERVICE_FILE_DEV ] 
+then
+	echo "DEV: Generating GoogleService-Info..."
+	base64 -D -o $GOOGLE_SERVICE_FILE_DEV <<< $GOOGLE_SERVICE_DEV
+fi
+
+if [ ! -f $SERVICE_CONFIG_FILE_DEV ] 
+then
+	echo "DEV: Generating ServiceConfig..."
+	base64 -D -o $SERVICE_CONFIG_FILE_DEV <<< $SERVICE_CONFIG_DEV
+fi
+
+if [ ! -f $GOOGLE_OAUTH2_FILE_PROD ] 
+then
+	echo "PROD: Generating GoogleOAuth2..."
+	base64 -D -o $GOOGLE_OAUTH2_FILE_PROD <<< $GOOGLE_OAUTH2_PROD
+fi
+
+if [ ! -f $GOOGLE_SERVICE_FILE_PROD ] 
+then
+	echo "PROD: Generating GoogleService-Info..."
+	base64 -D -o $GOOGLE_SERVICE_FILE_PROD <<< $GOOGLE_SERVICE_PROD
+fi
+
+if [ ! -f $SERVICE_CONFIG_FILE_PROD ] 
+then
+	echo "PROD: Generating ServiceConfig..."
+	base64 -D -o $SERVICE_CONFIG_FILE_PROD <<< $SERVICE_CONFIG_PROD
+fi
+
 
 echo "Stage: PRE-Xcode Build is completed..."
 
