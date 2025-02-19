9c11769ff954e649fe844c8a9e3b0e2c40406d3f
cat <zhouxl72@126.com>
2025-02-19 11:33:16 +0800

    fix： #819 receiver incorret warning when send

--- FRW/Modules/Wallet/Send/WalletSendAmountViewModel.swift
+++ FRW/Modules/Wallet/Send/WalletSendAmountViewModel.swift
@@ -79,24 +79,36 @@ final class WalletSendAmountViewModel: ObservableObject {
 
     // MARK: Internal
 
-    @Published private(set) var targetContact: Contact
-    @Published private(set) var token: TokenModel
-    @Published private(set) var amountBalance: Double = 0
-    @Published private(set) var coinRate: Double = 0
-    
-    @Published var inputText: String = ""
-    @Published private(set) var inputTokenNum: Double = 0
-    @Published private(set) var inputDollarNum: Double = 0
+    @Published
+    private(set) var targetContact: Contact
+    @Published
+    private(set) var token: TokenModel
+    @Published
+    private(set) var amountBalance: Double = 0
+    @Published
+    private(set) var coinRate: Double = 0
+
+    @Published
+    var inputText: String = ""
+    @Published
+    private(set) var inputTokenNum: Double = 0
+    @Published
+    private(set) var inputDollarNum: Double = 0
     private(set) var actualBalance: String = ""
 
-    @Published private(set) var exchangeType: WalletSendAmountView.ExchangeType = .token
-    @Published private(set) var errorType: WalletSendAmountView.ErrorType = .none
+    @Published
+    private(set) var exchangeType: WalletSendAmountView.ExchangeType = .token
+    @Published
+    private(set) var errorType: WalletSendAmountView.ErrorType = .none
 
-    @Published var showConfirmView: Bool = false
+    @Published
+    var showConfirmView: Bool = false
 
-    @Published private(set) var isValidToken: Bool = true
+    @Published
+    private(set) var isValidToken: Bool = true
 
-    @Published var isEmptyTransation = true
+    @Published
+    var isEmptyTransation = true
 
     var amountBalanceAsDollar: Double {
         coinRate * amountBalance
@@ -147,14 +159,16 @@ extension WalletSendAmountViewModel {
                     }
                     return
                 }
-                guard let compareKey = EVMAccountManager.shared.selectedAccount == nil ? token.contractId : token.flowIdentifier else {
+                guard let compareKey = EVMAccountManager.shared.selectedAccount == nil ? token
+                    .contractId : token.flowIdentifier else {
                     await MainActor.run {
                         self.isValidToken = false
                     }
                     return
                 }
-                let list = try await FlowNetwork.checkTokensEnable(address: Flow.Address(hex: address))
-                let model = list.first { $0.key.lowercased() == compareKey.lowercased() }
+                let list = try await FlowNetwork
+                    .checkTokensEnable(address: Flow.Address(hex: address))
+                let model = list.first { compareKey.lowercased().contains($0.key.lowercased()) }
                 let isValid = model?.value
                 await MainActor.run {
                     self.isValidToken = isValid ?? false
@@ -175,7 +189,7 @@ extension WalletSendAmountViewModel {
         defer {
             checkForInsufficientStorage()
         }
-        
+
         if errorType == .invalidAddress {
             return
         }
@@ -238,13 +252,16 @@ extension WalletSendAmountViewModel {
     }
 }
 
-// MARK: - InsufficientStorageToastViewModel
+// MARK: InsufficientStorageToastViewModel
 
 extension WalletSendAmountViewModel: InsufficientStorageToastViewModel {
     var variant: InsufficientStorageFailure? { _insufficientStorageFailure }
-    
+
     private func checkForInsufficientStorage() {
-        self._insufficientStorageFailure = insufficientStorageCheckForTransfer(amount: self.inputTokenNum.decimalValue, token: .ft(self.token))
+        _insufficientStorageFailure = insufficientStorageCheckForTransfer(
+            amount: inputTokenNum.decimalValue,
+            token: .ft(token)
+        )
     }
 }
 
@@ -386,8 +403,7 @@ extension WalletSendAmountViewModel {
                             amount: amount,
                             address: targetAddress
                         )
-                    }
-                    else if targetAddress == address {
+                    } else if targetAddress == address {
                         guard let vaultIdentifier = token.flowIdentifier else {
                             failureBlock()
                             return
@@ -398,8 +414,7 @@ extension WalletSendAmountViewModel {
                             fromEvm: true,
                             decimals: token.decimal
                         )
-                    }
-                    else {
+                    } else {
                         guard let bigUIntValue = amount.description
                             .parseToBigUInt(decimals: token.decimal),
                             let flowIdentifier = self.token.flowIdentifier
